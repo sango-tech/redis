5dab4c1fdc98ec5b7a6ca0cb5945c384de5def97
Binh Ho <abinh.ho@gmail.com>
2022-01-14 12:15:54 +0700

    bug: applet not found

--- redis/init.sh
+++ redis/init.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 #  WARNING you have Transparent Huge Pages (THP) support enabled in your kernel.
 #  This will create latency and memory usage issues with Redis.
 #  To fix this issue run the command 'echo never > /sys/kernel/mm/transparent_hugepage/enabled' as root,
